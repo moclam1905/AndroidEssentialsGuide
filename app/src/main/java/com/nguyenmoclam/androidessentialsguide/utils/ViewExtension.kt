fc131ffaa155f37fb770cdf77c5736deced37cf9
Lâm Nguyễn <moclam1905@gmail.com>
2025-02-26 20:40:28 +0700

    Refactor ViewExtension to simplify visibility logic in visibleIf function

--- app/src/main/java/com/nguyenmoclam/androidessentialsguide/utils/ViewExtension.kt
+++ app/src/main/java/com/nguyenmoclam/androidessentialsguide/utils/ViewExtension.kt
@@ -2,12 +2,11 @@ package com.nguyenmoclam.androidessentialsguide.utils
 
 import android.view.View
 
-fun View.visibleIf(condition: Boolean)  {
+fun View.visibleIf(condition: Boolean) {
     this.visibility =
-        if (condition)
-            {
-                View.VISIBLE
-            } else {
+        if (condition) {
+            View.VISIBLE
+        } else {
             View.GONE
         }
 }
